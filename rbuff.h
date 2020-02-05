a7d91253c70087a79bcbc2814ad0418b52b3274c
Martin Polasek <polasek.martin@seznam.cz>
2020-02-05 03:13:49 +0100

    peek, seek

--- rbuff.h
+++ rbuff.h
@@ -34,21 +34,25 @@ typedef struct
 
 /* Defines -------------------------------------------------------------------*/
 // Functions -------------------------------------------------------------------
-void rbuff_init(rbuff_t *rbuff, uint8_t *buff, uint32_t size);
 void rbuff_reset(rbuff_t *rbuff);
+void rbuff_init(rbuff_t *rbuff, uint8_t *buff, uint32_t size);
 
-uint32_t rbuff_cap(rbuff_t *rbuff);
+uint32_t rbuff_capacity(rbuff_t *rbuff);
 uint32_t rbuff_available(rbuff_t *rbuff);
 uint32_t rbuff_size(rbuff_t *rbuff);
 
-uint8_t rbuff_write(rbuff_t *rbuff, uint8_t *buff, uint32_t size);
-uint8_t rbuff_write_rb(rbuff_t *rbuff, rbuff_t *w_rbuff, uint32_t size);  //zrusit a nahradit moce a copy
-uint32_t rbuff_read(rbuff_t *rbuff, uint8_t *buff, uint32_t size);
+uint32_t rbuff_write(rbuff_t *rbuff, uint8_t *data, uint32_t len);
+uint8_t  rbuff_write_b(rbuff_t *rbuff, uint8_t data);
 
-uint8_t rbuff_copy(rbuff_t *rbuff_dst, rbuff_t *rbuff_src, uint32_t size);
-uint8_t rbuff_move(rbuff_t *rbuff_dst, rbuff_t *rbuff_src, uint32_t size);
+uint8_t rbuff_copy(rbuff_t *rbuff_dst, rbuff_t *rbuff_src, uint32_t len);
+uint8_t rbuff_move(rbuff_t *rbuff_dst, rbuff_t *rbuff_src, uint32_t len);
 
-uint8_t rbuff_write_b(rbuff_t *rbuff, uint8_t *data);
-uint8_t rbuff_read_b(rbuff_t *rbuff, uint8_t *data);
+uint32_t rbuff_read(rbuff_t *rbuff, uint8_t *buff, uint32_t len);
+uint8_t  rbuff_read_b(rbuff_t *rbuff, uint8_t *data);
+
+uint32_t rbuff_peek(rbuff_t *rbuff, uint8_t *buff, uint32_t len);
+uint32_t rbuff_peek_b(rbuff_t *rbuff, uint8_t *data);
+
+uint32_t rbuff_seek(rbuff_t *rbuff, uint32_t len);
 //------------------------------------------------------------------------------
 #endif /* RING_BUFFER_H_INCLUDED */
